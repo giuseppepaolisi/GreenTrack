97c64f385faac2d51afcfba4339ea4a8f5d7ce6c
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2026-02-24 11:24:40 +0100

    Update backend

--- backend/src/core/init.sql
+++ backend/src/core/init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS categories (
 );
 CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
+    name VARCHAR(20) NOT NULL UNIQUE,
     price DECIMAL(10,2) NOT NULL CHECK(price >0),
     quantity INTEGER NOT NULL CHECK(quantity>0) DEFAULT 0,
     category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
@@ -17,5 +17,4 @@ CREATE TABLE IF NOT EXISTS suppliers (
 );
 INSERT INTO categories (name, description) 
 VALUES ('elettronica', 'descrizione elettronica'), 
-       ('giardini', 'descrizione giardino')
-       ON CONFLICT (name) DO NOTHING;
+       ('giardini', 'descrizione giardino');
